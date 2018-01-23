e5e3470576627f2b07dd2132fd36f2b5fbb15dea
Artem Zhivoderov <artemz@retailnext.net>
2018-01-23 07:43:07 +0300

    Added integration test
    This test illustrates the 'double render' issue which was fixed via the
    previous commit.

--- tests/integration/components/time-picker-test.js
+++ tests/integration/components/time-picker-test.js
@@ -1,5 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { notEmpty, and } from '@ember/object/computed';
+import { isEmpty } from '@ember/utils';
 
 moduleForComponent('time-picker', 'Integration | Component | time picker', {
   integration: true
@@ -21,4 +23,26 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('it doesn\'t call "onchange" during the initial render', function(assert) {
+  this.set('selectedTime', undefined);
+  this.set('hasContent', notEmpty('selectedTime'));
+
+  // lostFocus is always false
+  this.set('lostFocus', false);
+
+  // noError is also always false but triggers 'propertyChanged' event when 'defaultTimeToNow' logic executes
+  this.set('noError', and('hasContent', 'lostFocus'));
+  this.set('onchange', function(dateTime) {
+    this.set('selectedTime', dateTime);
+  });
+
+  this.render(hbs`
+    <div no-error="{{noError}}">
+      {{time-picker class="{{classBinding}}" onchange=(action onchange)}}
+    </div>
+  `);
+
+  assert.notOk(isEmpty(this.get('selectedTime')));
 });
